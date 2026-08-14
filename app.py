b0e82803daeaa6eb8f40de3a59f754cc88c68419
Bjarnshargl <martin-hofstetter@gmx.de>
2026-08-14 18:51:32 +0200

    first steps

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from cs50 import SQL
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Configure CS50 Library to use SQLite database
+# db = SQL("sqlite:///heroes.db")
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
